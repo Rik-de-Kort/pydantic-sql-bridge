3851ff89b65ac1c9f780622df6b216b5c5e30f92
Rik <rik.de.kort@hey.com>
2023-10-26 23:35:09 +0200

    Initial (hairy) implementation of view support

--- pydantic_sql_bridge/sql_first.py
+++ pydantic_sql_bridge/sql_first.py
@@ -1,6 +1,9 @@
+from typing import Type, Annotated, ClassVar
+
+from pydantic import BaseModel
 from sqlglot import parse_one, expressions as expr
 
-from pydantic_sql_bridge.utils import get_model_name
+from pydantic_sql_bridge.utils import get_model_name, get_table_name, Annotations
 
 SqlglotType = expr.DataType.Type
 SQLGLOT_TYPE_TO_PYDANTIC = {
@@ -22,8 +25,7 @@ def transform_column_def(col_def: expr.ColumnDef) -> tuple[str, str]:
     return name, pydantic_type
 
 
-def parse_create_table(sql_stmt) -> tuple[str, list[tuple[str, str]]]:
-    sql_expr = parse_one(sql_stmt)
+def parse_create_table(sql_expr: expr.Create) -> tuple[str, list[tuple[str, str]]]:
     if not isinstance(sql_expr, expr.Create):
         raise ValueError(f'Statement not recognized as create table statement {sql_stmt=}')
 
@@ -34,8 +36,35 @@ def parse_create_table(sql_stmt) -> tuple[str, list[tuple[str, str]]]:
     return table_name, column_defs
 
 
-def to_pydantic_model(sql_stmt) -> str:
-    table_name, column_defs = parse_create_table(sql_stmt)
+# Todo: refactor this to take a dictionary of {table_name: (column_name, column_typestring)} instead of models
+def parse_create_view_joined(sql_expr: expr.Create, models: list[Type[BaseModel]]) -> tuple[str, list[tuple[str, str]]]:
+    join_source = sql_expr.expression.args['from']
+
+    alias_to_name = {join_source.this.alias: join_source.this.this.this} | {join.this.alias: join.this.this.this for
+                                                                            join in sql_expr.expression.args['joins']}
+    name_to_model = {get_table_name(model): model for model in models}
+    alias_to_model = {alias: name_to_model.get(name) for alias, name in alias_to_name.items()}
+
+    columns = sql_expr.expression.expressions
+    if any(missing_aliases := {col.table for col in columns if col.table not in alias_to_model}):
+        matching_names = {alias: alias_to_name.get(alias) for alias in missing_aliases}
+        raise ValueError(f'Cannot translate aliases {missing_aliases} to models. '
+                         f'Got name matches {matching_names} and models {name_to_model}')
+    column_defs = []
+    for col in columns:
+        col_name = col.this.this
+        col_model = alias_to_model[col.table].model_fields[col_name].annotation.__name__
+        column_defs.append((col_name, col_model))
+
+    view_name = sql_expr.this.this.this
+    return view_name, column_defs
+
+
+def to_pydantic_model(sql_expr: expr.Create) -> str:
+    if sql_expr.args['kind'] == 'VIEW':
+        table_name, column_defs = parse_create_view_joined(sql_expr)
+    else:
+        table_name, column_defs = parse_create_table(sql_expr)
     head = [f'class {get_model_name(table_name)}(BaseModel):',
             f'    query_name: ClassVar[str] = "{table_name}"']
     result = '\n'.join(head + [f'    {name}: {typ}' for name, typ in column_defs])
@@ -43,11 +72,45 @@ def to_pydantic_model(sql_stmt) -> str:
 
 
 def create_models_from_sql(sql: list[str]) -> str:
+    sql_exprs = [parse_one(sql_stmt) for sql_stmt in sql]
+    if any(non_create_exprs := [sql_expr for sql_expr in sql_exprs if not isinstance(sql_expr, expr.Create)]):
+        raise ValueError(f'Cannot parse {non_create_exprs} because they do not appear to be valid create expressions')
+    view_exprs = [sql_expr for sql_expr in sql_exprs if sql_expr.args['kind'] == 'VIEW']
+    table_exprs = [sql_expr for sql_expr in sql_exprs if sql_expr.args['kind'] != 'VIEW']
+
     to_join = [
-                  'from pydantic import BaseModel\n'
-                  'from pydantic_sql_bridge.sql_first import Annotations\n'
-                  'from typing import Annotated, ClassVar'
-              ] + [
-                  to_pydantic_model(sql_stmt) for sql_stmt in sql
-              ]
+        'from pydantic import BaseModel\n'
+        'from pydantic_sql_bridge.utils import Annotations\n'
+        'from typing import Annotated, ClassVar'
+    ]
+
+    model_names = []
+    for table_expr in table_exprs:
+        table_name, column_defs = parse_create_table(table_expr)
+        model_name = get_model_name(table_name)
+
+        head = [f'class {model_name}(BaseModel):',
+                f'    query_name: ClassVar[str] = "{table_name}"']
+        model_code = '\n'.join(head + [f'    {name}: {typ}' for name, typ in column_defs])
+        to_join.append(model_code)
+
+        # Make sure we can find these models in our views later
+        # Todo: ideally of course you map this properly or something...
+        exec(model_code)
+        model_names.append(model_name)
+
+    # You cannot put locals() inside the generator expression here,
+    # since it will only contain model_name and the iterator.
+    current_locals = locals()
+    models = [current_locals[model_name] for model_name in model_names]
+
+    for view_expr in view_exprs:
+        view_name, column_defs = parse_create_view_joined(view_expr, models)
+        model_name = get_model_name(view_name)
+
+        head = [f'class {model_name}(BaseModel):',
+                f'    query_name: ClassVar[str] = "{view_name}"']
+        model_code = '\n'.join(head + [f'    {name}: {typ}' for name, typ in column_defs])
+        to_join.append(model_code)
+
     return '\n\n'.join(to_join)
--- tests/test_sql_first.py
+++ tests/test_sql_first.py
@@ -8,19 +8,25 @@ def test_generate_models():
         sedol NCHAR(7) PRIMARY KEY,
         cluster NVARCHAR(50),
         n_invested BIGINT
+        CONSTRAINT FK_Sedol FOREIGN KEY (sedol) REFERENCES benchmark(sedol)
     )'''
 
     benchmark_sql = '''CREATE TABLE Benchmark (
-        sedol NCHAR(7),
+        sedol NCHAR(7) PRIMARY KEY,
         name NVARCHAR(50),
         n_available BIGINT,
         is_reit BIT,
-        CONSTRAINT FK_Sedol FOREIGN KEY (sedol) REFERENCES portfolio(sedol)
     )'''
+
+    master_sql = '''CREATE VIEW master AS
+    SELECT p.sedol, b.name, p.n_invested, b.n_available
+    FROM Portfolio p
+    JOIN Benchmark b ON p.sedol = b.sedol
+    '''
 
     expected = textwrap.dedent('''
     from pydantic import BaseModel
-    from pydantic_sql_bridge.sql_first import Annotations
+    from pydantic_sql_bridge.utils import Annotations
     from typing import Annotated, ClassVar
 
     class PortfolioRow(BaseModel):
@@ -31,10 +37,17 @@ def test_generate_models():
 
     class BenchmarkRow(BaseModel):
         query_name: ClassVar[str] = "Benchmark"
-        sedol: str
+        sedol: Annotated[str, Annotations.PRIMARY_KEY]
         name: str
         n_available: int
         is_reit: bool
+        
+    class MasterRow(BaseModel):
+        query_name: ClassVar[str] = "master"
+        sedol: str
+        name: str
+        n_invested: int
+        n_available: int
     ''')
-    actual = create_models_from_sql([portfolio_sql, benchmark_sql])
+    actual = create_models_from_sql([portfolio_sql, benchmark_sql, master_sql])
     assert actual.replace('\n', '').strip() == expected.replace('\n', '').strip()
