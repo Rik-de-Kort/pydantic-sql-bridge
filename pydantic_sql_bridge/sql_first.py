c37f3b7d5c13f19999a413e97b498be65bc9f5ec
Rik <rik.de.kort@hey.com>
2023-11-08 14:35:31 +0100

    Refactor view creation to take a model spec dictionary of dictionaries instead of BaseModel types

--- pydantic_sql_bridge/sql_first.py
+++ pydantic_sql_bridge/sql_first.py
@@ -1,15 +1,11 @@
 from collections import deque
-import datetime
-import typing
-from typing import Type
 
 import sqlglot.dialects
-from pydantic import BaseModel
-from sqlglot import parse_one, expressions as expr
+from sqlglot import parse_one, expressions as exp
 
-from pydantic_sql_bridge.utils import get_model_name, get_table_name, Annotations
+from pydantic_sql_bridge.utils import get_model_name
 
-SqlglotType = expr.DataType.Type
+SqlglotType = exp.DataType.Type
 SQLGLOT_TYPE_TO_PYDANTIC = {
     SqlglotType.BIGINT: 'int',
     SqlglotType.BIT: 'bool',
@@ -27,15 +23,15 @@ SQLGLOT_TYPE_TO_PYDANTIC = {
 }
 
 
-def transform_column_def(col_def: expr.ColumnDef) -> tuple[str, str]:
+def transform_column_def(col_def: exp.ColumnDef) -> tuple[str, str]:
     name = col_def.this.this
     pydantic_type = SQLGLOT_TYPE_TO_PYDANTIC[col_def.args['kind'].this]
 
     to_apply = deque()
     for constraint in col_def.args.get('constraints', []):
-        if isinstance(constraint.kind, expr.NotNullColumnConstraint) and constraint.kind.args.get('allow_null', False):
+        if isinstance(constraint.kind, exp.NotNullColumnConstraint) and constraint.kind.args.get('allow_null', False):
             to_apply.appendleft(lambda typ: f'typing.Optional[{typ}]')
-        if isinstance(constraint.kind, expr.PrimaryKeyColumnConstraint):
+        if isinstance(constraint.kind, exp.PrimaryKeyColumnConstraint):
             to_apply.append(lambda typ: f'typing.Annotated[{pydantic_type}, Annotations.PRIMARY_KEY]')
 
     for transform in to_apply:
@@ -43,17 +39,17 @@ def transform_column_def(col_def: expr.ColumnDef) -> tuple[str, str]:
     return name, pydantic_type
 
 
-def parse_create_table(sql_expr: expr.Create) -> tuple[str, list[tuple[str, str]]]:
-    if not isinstance(sql_expr, expr.Create):
+def parse_create_table(sql_expr: exp.Create) -> tuple[str, list[tuple[str, str]]]:
+    if not isinstance(sql_expr, exp.Create):
         raise ValueError(f'Statement not recognized as create table statement {sql_expr=}')
 
     table_name = sql_expr.this.this.this.this
     column_defs = []
     for sql_col_def in sql_expr.this.expressions:
-        if not isinstance(sql_col_def, expr.ColumnDef):
+        if not isinstance(sql_col_def, exp.ColumnDef):
             continue
         if any(
-                isinstance(constraint.kind, expr.GeneratedAsRowColumnConstraint) and constraint.kind.args.get('hidden')
+                isinstance(constraint.kind, exp.GeneratedAsRowColumnConstraint) and constraint.kind.args.get('hidden')
                 for constraint in sql_col_def.constraints
         ):
             continue
@@ -63,30 +59,33 @@ def parse_create_table(sql_expr: expr.Create) -> tuple[str, list[tuple[str, str]
     return table_name, column_defs
 
 
-def parse_first_table_name(sql_expr: expr.Expression) -> str:
-    if isinstance(sql_expr, expr.Column):
+def parse_first_table_name(sql_expr: exp.Expression) -> str:
+    if isinstance(sql_expr, exp.Column):
         return sql_expr.table
     else:
-        results = [node for node, parent, key in sql_expr.walk() if isinstance(node, expr.Column)]
+        results = [node for node, parent, key in sql_expr.walk() if isinstance(node, exp.Column)]
         if not results:
             raise ValueError(f'Cannot find column definition to get table from in {sql_expr}')
         return results[0].table
 
 
-def annotation_to_name(annotation: Type) -> str:
-    if annotation in (str, bool, int, float):
-        return annotation.__name__
-    return str(annotation)
+def strip_annotations(typ: str) -> str:
+    import re
+    match = re.match(r'typing.Annotated\[(.*), .*]', typ)
+    if match:
+        typ = match[1]
+    return typ
 
 
-# Todo: refactor this to take a dictionary of {table_name: (column_name, column_typestring)} instead of models
-def parse_create_view_joined(sql_expr: expr.Create, models: list[Type[BaseModel]]) -> tuple[str, list[tuple[str, str]]]:
+def parse_create_view_joined(
+        sql_expr: exp.Create,
+        models: dict[str, dict[str, str]]
+) -> tuple[str, list[tuple[str, str]]]:
     join_source = sql_expr.expression.args['from']
 
     alias_to_name = {join_source.this.alias_or_name: join_source.this.this.this}
     alias_to_name |= {join.this.alias_or_name: join.this.this.this for join in sql_expr.expression.args['joins']}
-    name_to_model = {get_table_name(model): model for model in models}
-    alias_to_model = {alias: name_to_model.get(name) for alias, name in alias_to_name.items()}
+    alias_to_model = {alias: models.get(name) for alias, name in alias_to_name.items()}
 
     columns = sql_expr.expression.expressions
     tables = [parse_first_table_name(col) for col in columns]
@@ -100,14 +99,14 @@ def parse_create_view_joined(sql_expr: expr.Create, models: list[Type[BaseModel]
         col_name = col.this
         while not isinstance(col_name, str):  # Todo: should we parse this out properly?
             col_name = col_name.this
-        col_model = annotation_to_name(alias_to_model[table].model_fields[col_name].annotation)
+        col_model = strip_annotations(alias_to_model[table][col_name])
         column_defs.append((col_name, col_model))
 
     view_name = sql_expr.this.this.this
     return view_name, column_defs
 
 
-def to_pydantic_model(sql_expr: expr.Create) -> str:
+def to_pydantic_model(sql_expr: exp.Create) -> str:
     if sql_expr.args['kind'] == 'VIEW':
         table_name, column_defs = parse_create_view_joined(sql_expr)
     else:
@@ -120,7 +119,7 @@ def to_pydantic_model(sql_expr: expr.Create) -> str:
 
 def create_models_from_sql(sql: list[str], dialect: sqlglot.Dialects = sqlglot.dialects.SQLite) -> str:
     sql_exprs = [parse_one(sql_stmt, dialect=dialect) for sql_stmt in sql]
-    if any(non_create_exprs := [sql_expr for sql_expr in sql_exprs if not isinstance(sql_expr, expr.Create)]):
+    if any(non_create_exprs := [sql_expr for sql_expr in sql_exprs if not isinstance(sql_expr, exp.Create)]):
         raise ValueError(f'Cannot parse {non_create_exprs} because they do not appear to be valid create expressions')
     view_exprs = [sql_expr for sql_expr in sql_exprs if sql_expr.args['kind'] == 'VIEW']
     table_exprs = [sql_expr for sql_expr in sql_exprs if sql_expr.args['kind'] != 'VIEW']
@@ -131,30 +130,24 @@ def create_models_from_sql(sql: list[str], dialect: sqlglot.Dialects = sqlglot.d
         'import typing'
     ]
 
-    model_names = []
+    model_specs = {}
     for table_expr in table_exprs:
         table_name, column_defs = parse_create_table(table_expr)
-        model_name = get_model_name(table_name)
+        # Make sure we can find these models in our views later
+        model_specs[table_name] = dict(column_defs)
 
+        model_name = get_model_name(table_name)
         head = [f'class {model_name}(BaseModel):',
                 f'    query_name: typing.ClassVar[str] = "{table_name}"']
+
         model_code = '\n'.join(head + [f'    {name}: {typ}' for name, typ in column_defs])
         to_join.append(model_code)
+
         if 'datetime' in model_code and 'datetime' not in to_join[0]:
             to_join[0] = 'import datetime\n' + to_join[0]
 
-        # Make sure we can find these models in our views later
-        # Todo: ideally of course you map this properly or something...
-        exec(model_code)
-        model_names.append(model_name)
-
-    # You cannot put locals() inside the generator expression here,
-    # since it will only contain model_name and the iterator.
-    current_locals = locals()
-    models = [current_locals[model_name] for model_name in model_names]
-
     for view_expr in view_exprs:
-        view_name, column_defs = parse_create_view_joined(view_expr, models)
+        view_name, column_defs = parse_create_view_joined(view_expr, model_specs)
         model_name = get_model_name(view_name)
 
         head = [f'class {model_name}(BaseModel):',
